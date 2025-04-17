dfc598f74a0f39fe5486e9bb403772da8a868665
Taha969 <taha1428mm@gmail.com>
2025-04-17 23:59:59 +0200

    AI.PY

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2
+import easyocr
+
+img = cv2.imread('AI.py.png')
+
+reader = easyocr.Reader(['en'] , gpu=False)
+result = reader.readtext(img)
+
+for text in result:
+    p1 = text[0][0]
+    p2 = text[0][2]
+    detctedText = text[1]
+    #print(detctedText)
+
+
+    cv2.rectangle(img, p1, p2,  (0, 255, 0 ), 3)
+    cv2.putText(img, detctedText, p1, cv2.FONT_HERSHEY_COMPLEX, 0.75,(255, 0,150 ), 3)
+
+cv2.imshow('Image', img)
+cv2.waitKey(0)
